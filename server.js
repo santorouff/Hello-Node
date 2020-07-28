6888614bca3f7475bdde39e0f67b1fb9f37f5076
santorouff <santoro_uff@yahoo.com.br>
2020-07-28 10:29:20 -0300

    third commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 
+var port = process.env.PORT || 8080;
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
-}).listen(8080);
+}).listen(port);
