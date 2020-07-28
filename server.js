5769b8d4391c22301daeb8bb7c9a806a7beef4b4
santorouff <santoro_uff@yahoo.com.br>
2020-07-28 10:59:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;  // variável de ambiente do Heroku para armazenar a porta
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
